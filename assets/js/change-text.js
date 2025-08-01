0d8ee82b24e8c59892e7a514de6d7058b42a7248
devgabrielnascimento <devgabrielnascimento@gmail.com>
2025-07-31 23:29:32 -0300

    style: add text change effect in headings at container section info

---
+++ assets/js/change-text.js
@@ -0,0 +1,8 @@
+const typed = new Typed(".multiple-text", {
+  strings: ["Desenvolvedor Full Stack!", "Amo cloud computing!", "e estou sempre aprendendo!"],
+  typeSpeed: 70,
+  backSpeed: 70,
+  backDelay: 1000,
+  loop: true,
+  cursor:"/assets/images/cursor_actived.svg",
+});
